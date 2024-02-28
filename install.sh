cbe2eb10c640b536a732e8edd2b85374333b9077
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-02-28 02:12:37 -0300

    Fixed install.sh bug

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
+#! /bin/bash
+read -sp "Introduce tu contraseña: " password
 rutaArchivos=$(pwd) 
 echo "Instalando la fuente:FiraCode Nerd Font"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip > /tmp/
-cd /tmp/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip 
+mv FiraCode.zip /tmp
+cd /tmp
 unzip FiraCode.zip
-mv *.ttf /usr/share/fonts/truetype/
+echo $password | sudo -S mv *.ttf /usr/share/fonts/truetype/
 cd $rutaArchivos
 echo "Instalando configración de i3..."
-cp ./i3/config ~/.config/i3/
-cp ./i3/powermenu.sh ~/.config/i3/
+cp ./i3/config ~/.config/i3/config
+cp ./i3/powermenu.sh ~/.config/i3/powermenu.sh
 
 echo "Instalando configuración de i3status..."
-cp ./i3status/config ~/.config/i3staus/
+cp -r ./i3status/config ~/.config/i3status/config
 
 echo "Instalación completada: recarga el gestor de ventanas i3 con Supr+Shift+r"
